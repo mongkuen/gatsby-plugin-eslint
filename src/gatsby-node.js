ac3d08e3acb608a08488d9e3dee0ff40e2c13a8d
Abzeede(Bank) <purijit@satang.com>
2018-08-02 10:42:07 +0700

    fix bugs: cannot run command gatsby build

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,4 +1,4 @@
-exports.modifyWebpackConfig = ({ config, stage }, pluginOptions) => {
+exports.onCreateWebpackConfig = ({ config, stage }, pluginOptions) => {
   const test = pluginOptions.test || /\.js$|\.jsx$/;
   const exclude = pluginOptions.exclude || /(node_modules|cache|public)/;
   const options = pluginOptions.options || {};
